a5dfa8a2b688641ddeeb15d7ee2f32374d0c4dc4
ReflexTen-dev <bulanov.roman4@gmail.com>
2022-01-29 22:44:06 +0500

    added styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,9 @@
 $(function () {
 
   $('.header__slider').slick({
-    prevArrow: '<button type="button" class="slick-prev"><svg class="icon-arrow icon-arrow--left"><use xlink:href="images/sprite.svg#icon-arrow-left"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg class="icon-arrow icon-arrow--right"><use xlink:href="images/sprite.svg#icon-arrow-right"></use></svg></button>'
+    prevArrow: '<button type="button" class="slider-btn slider-btn__left"><svg class="icon-arrow icon-arrow--left"><use xlink:href="images/sprite.svg#icon-arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slider-btn slider-btn__right"><svg class="icon-arrow icon-arrow--right"><use xlink:href="images/sprite.svg#icon-arrow-right"></use></svg></button>',
+    infinite: false
   })
 
   var mixer = mixitup('.products');

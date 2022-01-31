94cb576512a0b7aadc29ed31a82bc55b65e5daed
ReflexTen-dev <bulanov.roman4@gmail.com>
2022-01-31 23:20:01 +0500

    correct mistakes

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-  $('.header__slider').slick({
+  $('.top-slider__wrapper').slick({
     prevArrow: '<button type="button" class="slider-btn slider-btn__left"><svg class="icon-arrow icon-arrow--left"><use xlink:href="images/sprite.svg#icon-arrow-left"></use></svg></button>',
     nextArrow: '<button type="button" class="slider-btn slider-btn__right"><svg class="icon-arrow icon-arrow--right"><use xlink:href="images/sprite.svg#icon-arrow-right"></use></svg></button>',
     infinite: false

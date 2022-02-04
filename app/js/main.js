6e69e7343d3a01a0a8e868e6b2363e7a6b4503ec
ReflexTen-dev <bulanov.roman4@gmail.com>
2022-02-04 22:42:22 +0500

    added style

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,29 @@ $(function () {
     infinite: false
   })
 
-  var mixer = mixitup('.top-product__inner');
+
+
+  var containerEl1 = document.querySelector('[data-ref="mix1"]');
+  var containerEl2 = document.querySelector('[data-ref="mix2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  }
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer1 = mixitup(containerEl2, config);
+
+
+  // var config = {
+  //   controls: {
+  //     scope: 'local'
+  //   }
+  // }
+
+  // var mixer = mixitup('.top-product__inner, config');
+  // var mixer = mixitup('.promo-stocks__inner, config');
+
 
 })
